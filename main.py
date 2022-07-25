81bbc3f98fe49994f63e86206139bc0eabd3d02a
kifuan <kifuan@foxmail.com>
2022-07-25 21:10:34 +0800

    init repo

---
+++ main.py
@@ -0,0 +1,12 @@
+from txtparser import parse
+from analysis import plot
+
+
+def main(name: str, qid: str, limit: int = 30):
+    parse(f'{name}.txt', f'{name}.json')
+    plot(f'{name}.json', qid, limit)
+
+
+if __name__ == '__main__':
+    main('xxx', 'test')
+
