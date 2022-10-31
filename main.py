9539ef513a094141c930ffc9d0e9c753034a5f39
kifuan <kifuan@foxmail.com>
2022-10-31 22:20:29 +0800

    feat: parse console arguments

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from analysis import plot, parser_table
 def parse_args():
     parser = argparse.ArgumentParser(description='Word counter for QQ')
     parser.add_argument('-f', '--file', action='store', dest='file', required=True, help='The chat-history file.')
-    parser.add_argument('-i', '--qid', action='store', dest='qid', required=True, help='The QQ id you want to analyze.')
-    parser.add_argument('-l', '--limit', action='store', dest='limit', default=30, help='The limit of words.')
+    parser.add_argument('-q', '--qid', action='store', dest='qid', required=True,
+                        help='The QQ id you want to analyze. It should be QQ code when '
+                             'mode is group, and name when mode is friend.')
+    parser.add_argument('-l', '--limit', action='store', dest='limit', type=int, default=30, help='The limit of words.')
     parser.add_argument('-m', '--mode', action='store', dest='mode', default=parser_table.default_mode,
                         help='The parser mode.', choices=parser_table.keys())
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    path, mode, qid, limit = sys.argv[1:]
-    limit = int(limit)
-    plot(path, mode, qid, limit)
+    args = parse_args()
+    plot(args.file, args.mode, args.qid, args.limit)
