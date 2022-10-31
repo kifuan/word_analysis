500d469133874d5408accd2c2dec100d9a08b339
kifuan <kifuan@foxmail.com>
2022-10-31 21:20:59 +0800

    feat: parser can get display name for qid

--- analysis/parser.py
+++ analysis/parser.py
@@ -18,18 +18,28 @@ class MessageParser(ABC):
     state_specific = StateSpecificMethod()
 
     def __init__(self):
+        """
+        Note that if subclass overrides __init__ and it takes arguments,
+        please rewrite setup_vars as well.
+        """
         self.state = State.EMPTY
-        self.last_id = ''
+        self.last_qid = ''
         self.line_data: LineData = {}
         self.line_number = 0
         self.lines = []
 
-    # Just make the code less strange, because we should initialize
-    # properties before parsing, and calling __init__ is strange.
-    setup_vars = __init__
+    def setup_vars(self):
+        # Just calls the __init__ method to set up variables,
+        # and the subclass should override this method to
+        # initialize if its __init__ takes arguments.
+        self.__init__()
 
     @abstractmethod
-    def extract_id(self, line: str, line_number: int) -> str:
+    def extract_qid(self, line: str, line_number: int) -> str:
+        raise NotImplementedError()
+
+    @abstractmethod
+    def get_display_name(self, qid: str) -> str:
         raise NotImplementedError()
 
     @staticmethod
@@ -47,7 +57,7 @@ class MessageParser(ABC):
             return
 
         # It is the first line, but it does not contain id, so just skip it.
-        if self.last_id == '':
+        if self.last_qid == '':
             self.line_number += 1
             return
 
@@ -55,8 +65,8 @@ class MessageParser(ABC):
 
     @state_specific.register(State.ID)
     def _(self):
-        self.last_id = self.extract_id(self.line, self.line_number)
-        self.line_data.setdefault(self.last_id, [])
+        self.last_qid = self.extract_qid(self.line, self.line_number)
+        self.line_data.setdefault(self.last_qid, [])
         self.state = State.MESSAGE
         self.line_number += 1
         self.state = State.EMPTY
@@ -66,7 +76,7 @@ class MessageParser(ABC):
         if starts_with_date(self.line):
             self.state = State.ID
             return
-        self.line_data[self.last_id].append(self.line)
+        self.line_data[self.last_qid].append(self.line)
         self.line_number += 1
         self.state = State.EMPTY
 
@@ -88,23 +98,42 @@ def starts_with_date(line: str) -> bool:
     return bool(re.search(r'^(\d{4}-\d{2}-\d{2}.+)', line))
 
 
+ANGLE_BRACKETS = re.compile(r'<(.*?)>')
+BRACKETS = re.compile(r'[(](.*?)[)]')
+
+
 @parser_table.register('group')
 class GroupMessageParser(MessageParser):
-    def extract_id(self, line: str, line_number: int) -> str:
+    def __init__(self):
+        super().__init__()
+        self.display_name = {}
+
+    def update_display_name(self, qid: str, line: str, lb: str, rb: str) -> str:
+        # Remove the bracket with qid in it.
+        # If the username contains such pattern, it will be removed as well.
+        # I don't think this is problematic, because the name is used to display.
+        remove_qid = re.compile(f'[{lb}]({qid})[{rb}]')
+        self.display_name[qid] = REMOVE_DATE.sub('', remove_qid.sub('', line)).strip()
+        return qid
+
+    def extract_qid(self, line: str, line_number: int) -> str:
         # Try to find it with ().
-        brackets = re.findall(r'[(](.*?)[)]', line)
+        brackets = BRACKETS.findall(line)
         if brackets:
-            return brackets[-1]
+            return self.update_display_name(brackets[-1], line, '(', ')')
 
         # Try to find it with <>.
-        angel_brackets = re.findall(r'<(.*?)>', line)
+        angel_brackets = ANGLE_BRACKETS.findall(line)
         if angel_brackets:
-            return angel_brackets[-1]
+            return self.update_display_name(angel_brackets[-1], line, '<', '>')
 
         raise ScanningError(
             f'Cannot find qid or email in line {line_number + 1}, but this line'
             'starts with regular datetime, which is what we want.'
         )
+
+    def get_display_name(self, qid: str) -> str:
+        return self.display_name[qid]
 
 
 REMOVE_DATE = re.compile(r'^(\d{4}-\d{2}-\d{2}\s+\d\d?:\d{2}:\d{2}\s*)')
@@ -112,5 +141,10 @@ REMOVE_DATE = re.compile(r'^(\d{4}-\d{2}-\d{2}\s+\d\d?:\d{2}:\d{2}\s*)')
 
 @parser_table.register('friend')
 class FriendMessageParser(MessageParser):
-    def extract_id(self, line: str, line_number: int) -> str:
+    def extract_qid(self, line: str, line_number: int) -> str:
         return REMOVE_DATE.sub('', line)
+
+    def get_display_name(self, qid: str) -> str:
+        # The friend parser uses displaying name as id,
+        # so just returning qid works.
+        return qid
